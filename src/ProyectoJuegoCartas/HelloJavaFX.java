da5842c4515d8c832ba1ad90bdc91b3687f7c575
javi <viprin18@gmail.com>
2025-07-19 17:01:54 +0200

    MiProyectoVideojuegoCartas continuando 19/07/2025

--- src/ProyectoJuegoCartas/HelloJavaFX.java
+++ src/ProyectoJuegoCartas/HelloJavaFX.java
@@ -3,6 +3,7 @@ package ProyectoJuegoCartas;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,7 +16,11 @@ public class HelloJavaFX extends Application {
         StackPane root = new StackPane(btn);
         Scene scene = new Scene(root, 400, 300);
 
+
         primaryStage.setTitle("Mi primer JavaFX");
+        Image cartasIcono = new Image(getClass().getResourceAsStream("/icono.png")); //No encuentra la ubicación de la imagen
+                                                                                            // (esta en el packete resources)
+        primaryStage.getIcons().add(cartasIcono);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
